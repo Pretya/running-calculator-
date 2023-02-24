51fe8731a9593a0053675b6880ee37ee975e9c32
Yurij Shulika <105563150+Pretya@users.noreply.github.com>
2023-02-24 19:46:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-function runDistance(){
-  const burned = document.querySelector('.burned');
-  const result = document.getElementById("distance-weight").value * document.getElementById("distance-run").value;
+let weight = document.getElementById("distance-weight").value;
+let distance = document.getElementById("distance-run").value;
+let heartWeight = document.getElementById("heart-weight").value;
+let runningTime = document.getElementById("running-time").value;
+let runningDistanceTime = document.getElementById("distance-run-time").value;
 
-  burned.innerHTML = `
-  <p>${result} calories</p>
-  `
+function runDistance(weight, distance){
+  return weight * distance;
+}
+
+function runDistanceRenderOne() {
+  const burnedTextOne = document.querySelector('.burned');
+  burnedTextOne.innerHTML =  runDistance(document.getElementById("distance-weight").value, document.getElementById("distance-run").value);
+
 }
 
 
-function runDistanceHeart(){
-  const burned = document.querySelector('.burned-pulse');
-  const result = 0.75 * document.getElementById("heart-weight").value * document.getElementById("distance-run-time").value * document.getElementById("running-time").value;
-  
-  burned.innerHTML = `
-  <p>${result} calories</p>
-  `
+function runDistanceHeart(heartWeight, runningTime, runningDistanceTime){
+  return 0.75 * heartWeight * runningTime * runningDistanceTime;
+}
+
+function runDistanceRenderTwo() {
+  const burnedTextTwo = document.querySelector('.burned-pulse');
+  burnedTextTwo.innerHTML = runDistanceHeart(document.getElementById("heart-weight").value, document.getElementById("running-time").value, document.getElementById("distance-run-time").value)
 }
